ddba4f50ca7140eb51300be32e05fb79929a07bf
Akshaypal Bishnoi <akshaypal187829@gmail.com>
2026-01-29 23:15:02 +0530

    add skills header

--- client/src/pages/Skills.jsx
+++ client/src/pages/Skills.jsx
@@ -1,8 +1,16 @@
 import SkillsTabs from "../components/SkillsTabs"
+import { motion } from "framer-motion"
 
 export default function Skills() {
   return (
     <section id="skills" className="py-24 px-4 ">
+      <motion.h2
+        whileInView={{ opacity: 1, y: 0 }}
+        initial={{ opacity: 0, y: 40 }}
+        className="text-3xl font-bold text-center mb-10"
+      >
+        Skills
+      </motion.h2>
 
       <SkillsTabs />
     </section>
